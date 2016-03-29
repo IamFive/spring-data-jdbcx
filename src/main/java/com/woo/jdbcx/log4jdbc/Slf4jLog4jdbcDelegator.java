a6c082072d66804dc56d9cf5866bbc18017ff44d
woo <xmufive@gmail.com>
2016-03-29 17:20:59 +0800

    disable not support feature log

--- src/main/java/com/woo/jdbcx/log4jdbc/Slf4jLog4jdbcDelegator.java
+++ src/main/java/com/woo/jdbcx/log4jdbc/Slf4jLog4jdbcDelegator.java
@@ -54,6 +54,8 @@ public class Slf4jLog4jdbcDelegator extends Slf4jSpyLogDelegator {
 	public void exceptionOccured(Spy spy, String methodCall, Exception e, String sql, long execTime) {
 		if (e instanceof SQLFeatureNotSupportedException) {
 			// disable not support feature exception
+		} else if (e.getMessage().contains("unsupported")) {
+			// disable not support message
 		} else {
 			super.exceptionOccured(spy, methodCall, e, sql, execTime);
 		}
